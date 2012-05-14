d1fee05d02b07ce9fa6de4d607b7a64a94d00d1f
Bjarte Stien Karlsen <bjarte@bjartek.org>
2012-05-14 18:32:05 +0200

    finished step 2

---
+++ app/models/Company.scala
@@ -0,0 +1,23 @@
+package models
+
+import play.api.Play.current
+import com.novus.salat._
+import com.novus.salat.dao._
+import com.mongodb.casbah.Imports._
+import se.radley.plugin.salat._
+
+case class Company(
+  id: ObjectId = new ObjectId, 
+  name: String
+)
+
+object Company extends ModelCompanion[Company, ObjectId] {
+  val collection = mongoCollection("companies")
+  val dao = new SalatDAO[Company, ObjectId](collection = collection) {}
+
+  def options: Seq[(String,String)] = {
+    find(MongoDBObject.empty).map(c => (c.id.toString, c.name)).toSeq
+  }
+}
+
+// vim: set ts=2 sw=2 et:
---
+++ app/models/Computer.scala
@@ -0,0 +1,31 @@
+package models
+
+import play.api.Play.current
+import com.novus.salat._
+import com.novus.salat.dao._
+import com.mongodb.casbah.Imports._
+import se.radley.plugin.salat._
+
+case class Computer(
+  id: ObjectId = new ObjectId, 
+  name: String, 
+  introduced: Option[Date] = None, 
+  discontinued: Option[Date] = None, 
+  @Key("company_id") companyId: Option[Long])
+
+object Computer extends ModelCompanion[Computer, ObjectId] {
+  val collection = mongoCollection("computers")
+  val dao = new SalatDAO[Computer, ObjectId](collection = collection) {}
+
+  def list(filter: String = ""): List[(Computer, Option[Company])] = {
+    val where = if(filter == "") MongoDBObject.empty else MongoDBObject("name" ->(""".*"""+ filter+""".*""").r)
+    find(where).map(withCompany).toList
+  }
+
+  def withCompany(computer:Computer) = {
+    val company = computer.companyId.flatMap(id => Company.findOneByID(id))
+    (computer, company)
+  }
+}
+
+// vim: set ts=2 sw=2 et:
